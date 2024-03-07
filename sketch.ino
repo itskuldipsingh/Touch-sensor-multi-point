c9e56db3bfaf73e824d7b228ed7f33eac94cdedf
Kuldip Singh <159125809+itskuldipsingh@users.noreply.github.com>
2024-03-07 11:14:45 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,17 +1,36 @@
-#define led D5
-#define touch1 D0
-#define touch2 D1
-#define touch3 D2
+#define fan D6
+#define led D7
 
-void setup(){
+#define touch1_1 D0
+#define touch1_2 D1
+#define touch2_1 D2
+#define touch2_2 D3
+#define touch3_1 D4
+#define touch3_2 D5
+
+void setup() {
+  pinMode(fan, OUTPUT);
   pinMode(led, OUTPUT);
-  pinMode(touch1, INPUT);
-  pinMode(touch2, INPUT);
-  pinMode(touch3, INPUT);
+
+  digitalWrite(fan, LOW);
+  digitalWrite(led, LOW);
+  
+  pinMode(touch1_1, INPUT);
+  pinMode(touch1_2, INPUT);
+  pinMode(touch2_1, INPUT);
+  pinMode(touch2_2, INPUT);
+  pinMode(touch3_1, INPUT);
+  pinMode(touch3_2, INPUT);
+  
+  Serial.begin(9600);
 }
 
-void loop(){
-  if ((digitalRead(touch1) == HIGH) || (digitalRead(touch2) == HIGH) || (digitalRead(touch3) == HIGH)) {
+void loop() {
+  if ((digitalRead(touch1_1) == HIGH) || (digitalRead(touch2_1) == HIGH) || (digitalRead(touch3_1) == HIGH)) {
+    digitalWrite(fan, !digitalRead(fan));
+    delay(500);
+  }
+  if ((digitalRead(touch1_2) == HIGH) || (digitalRead(touch2_2) == HIGH) || (digitalRead(touch3_2) == HIGH)) {
     digitalWrite(led, !digitalRead(led));
     delay(500);
   }
